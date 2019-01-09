b832096607b05c918d78bd0934d1f1ed858b893a
David Niembro <david@niembro.ch>
2019-01-09 09:32:19 +0100

    add axios and fetch quizz from json file

---
+++ Qwizz/src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        {{qwizz.name}}
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        qwizz:{
+            type: Object,
+        }
+    }
+}
+</script>
+<style lang="css"scoped>
+
+.card {
+    height:150px;
+    width:150px;
+    background-color:white;
+    filter: drop-shadow(0 0 0.25rem grey);
+    margin-top: 20px
+}
+</style>
