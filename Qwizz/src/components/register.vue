ca1de04fd68598d0cef35077bd3b9e9fa26165dc
JarodS <strecky1800@gmail.com>
2019-01-14 15:00:09 +0100

    Added vue-router

---
+++ Qwizz/src/components/register.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card">
+        {{qwizz.name}}
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        qwizz:{
+            type: Object,
+        }
+    }
+}
+</script>
+<style lang="css"scoped>
+
+.card {
+    height:150px;
+    width:150px;
+    background-color:white;
+    filter: drop-shadow(0 0 0.35rem lightgray);
+    margin-left: 20px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 15px;
+}
+</style>
