f760a6e44635ae5337f1f5ea485cb67aee8cd3aa
David Niembro <david@niembro.ch>
2019-01-14 15:49:44 +0100

    Add routes

--- Qwizz/src/main.js
+++ Qwizz/src/main.js
@@ -1,13 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import login from './components/login'
+import register from './components/register'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+const routes = [   
+    {path: '/login', component: login},
+    {path: '/register', component: register},
+]
+
+const router = new VueRouter({
+    routes,
+    mode:'history'
 })
+
+new Vue({
+  router
+}).$mount('#app');
