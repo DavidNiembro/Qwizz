f8ca9b698aa39d5e2c98cf0305960356f8378059
Jarod Streckeisen <strecky1800@gmail.com>
2019-01-30 11:26:39 +0100

    Update register css

--- src/components/Auth/register.vue
+++ src/components/Auth/register.vue
@@ -49,7 +49,7 @@ font-size: 14px;
 font-family: "Roboto", sans-serif;
 text-transform: uppercase;
 outline: 0;
-background: #4CAF50;
+background: #df091d;
 width: 100%;
 border: 0;
 padding: 15px;
@@ -60,7 +60,7 @@ transition: all 0.3 ease;
 cursor: pointer;
 }
 .form button:hover,.form button:active,.form button:focus {
-background: #43A047;
+background: #df091d;
 }
 .form .message {
 margin: 15px 0 0;
@@ -68,7 +68,7 @@ color: #b3b3b3;
 font-size: 12px;
 }
 .form .message a {
-color: #4CAF50;
+color: #df091d;
 text-decoration: none;
 }
 
@@ -106,11 +106,11 @@ text-decoration: none;
 color: #EF3B3A;
 }
 body {
-background: #76b852; /* fallback for old browsers */
-background: -webkit-linear-gradient(right, #76b852, #8DC26F);
-background: -moz-linear-gradient(right, #76b852, #8DC26F);
-background: -o-linear-gradient(right, #76b852, #8DC26F);
-background: linear-gradient(to left, #76b852, #8DC26F);
+background: #df091d; /* fallback for old browsers */
+background: -webkit-linear-gradient(right, #df091d, #df091d);
+background: -moz-linear-gradient(right, #df091d, #df091d);
+background: -o-linear-gradient(right, #df091d, #df091d);
+background: linear-gradient(to left, #df091d, #df091d);
 font-family: "Roboto", sans-serif;
 -webkit-font-smoothing: antialiased;
 -moz-osx-font-smoothing: grayscale;
